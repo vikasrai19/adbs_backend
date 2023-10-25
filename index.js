951c04c1d67e9e30d2c46d8fd4220c0f34aa5003
Vikas Rai J <vikasraij@Vikass-Air.bbrouter>
2023-10-25 13:14:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Welcome to ABDS Project backend')
+})
+
+app.listen(port, () => {
+    console.log(`Application started running at http://localhost:${port}`);
+})
