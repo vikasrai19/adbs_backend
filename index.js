b44c4efb6767eda40c058b93af579eed6ed132ba
srujanshettythor <shrujanushetty@gmail.com>
2023-12-05 13:09:43 +0530

    busboardingpoints endpoints

--- index.js
+++ index.js
@@ -248,6 +248,42 @@ app.post('/web/api/addstudent', (req, res) => {
   });
 })
 
+app.post('/web/api/busboardingpoints', (req, res) => {
+  const {boardingTime,dropTime,userId} = req.body;
+  db.query('select userId from Users u, usertype t where u.usertype_id = t.usertype_id and UserId=? and t.usertype = "Admin"', [userId], (er, rw, fl) => {
+    if (er) {
+      res.status(400).json({ "message": "Invalid user" });
+    }
+    if (rw.length === 0) {
+      res.status(400).json({ "message": "Invalid user" });
+    } else {
+      db.query('SELECT * FROM busboardingpoints WHERE boardingTime = ? OR	dropTime = ?', [boardingTime, dropTime], (err, rows) => {
+        if (err) {
+          res.status(400).send(err.message);
+        } else {
+          if (rows.length > 0) {
+            console.log("Data already exists");
+            res.status(400).json({ "message": "Data already exists" });
+          } else {
+            const acId = crypto.randomUUID()
+             const bp="3a976c5d88354918b3733511fe837216";
+             const ci="37902748ffbe47969";
+             const ac="6bc54b4e35b249e3964cf5d11970a685";
+            db.query('INSERT INTO busboardingpoints(busBoardingPointId,collegeBusId,academicyearId,boardingPointId,boardingTime,dropTime) VALUES (?, ?, ?,?,?,?)', [acId,ci,ac,bp,boardingTime,dropTime], (err, result) => {
+              if (err) {
+                res.status(400).send(err.message);
+              } else {
+                res.status(200).json({ "message": "Data inserted successfully" });
+              }
+            });
+          }
+        }
+      });
+    }
+  });
+})
+
+
 
 app.get('/web/api/academicyear', (req, res) => {
   db.query('select * from academicyear', (err, result, fields) => {
--- tstlogin.js
+++ tstlogin.js
@@ -59,59 +59,43 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'nittibuscheck'
+  database: 'nittebuscheck'
 });
 
 // Sample data to insert
 const busboardingpointData = {
-  collegeBusId: "37902748-ffbe-4796-9", // Assuming the collegeBusId you want to insert
-  academicyearId: "6bc54b4e-35b2-49e3-964c-f5d11970a685", // Assuming the academicyearId you want to insert
-  boardingPointId: "3a976c5d-8835-4918-b373-3511fe837216", // Assuming the boardingPointId you want to insert
+  collegeBusId: 1, // Assuming the collegeBusId you want to insert
+  academicyearId: 2023, // Assuming the academicyearId you want to insert
+  boardingPointId: 10, // Assuming the boardingPointId you want to insert
   boardingTime: '08:00:00', // Replace with the boarding time you want to insert
   dropTime: '16:00:00' // Replace with the drop time you want to insert
 };
 
-// Function to insert data into busboardingpoints table
-const insertData = async () => {
-  try {
-    // Check if the referenced IDs exist in their respective tables before insertion
-    const [collegebusExists] = await connection.query('SELECT 1 FROM collegebus WHERE collegeBusId = ?', [busboardingpointData.collegeBusId]);
-    const [academicyearExists] = await connection.query('SELECT 1 FROM academicyear WHERE academicyearId = ?', [busboardingpointData.academicyearId]);
-    const [boardingpointExists] = await connection.query('SELECT 1 FROM boardingpoints WHERE boardingPointId = ?', [busboardingpointData.boardingPointId]);
+// Insert query
+const insertQuery = 'INSERT INTO busboardingpoints (collegeBusId, academicyearId, boardingPointId, boardingTime, dropTime) VALUES (?, ?, ?, ?, ?)';
 
-    if (!collegebusExists || !academicyearExists || !boardingpointExists) {
-      console.error('One or more referenced IDs do not exist in their respective tables.');
-      return;
-    }
-
-    // Insert query
-    const insertQuery = 'INSERT INTO busboardingpoints (collegeBusId, academicyearId, boardingPointId, boardingTime, dropTime) VALUES (?, ?, ?, ?, ?)';
-
-    // Perform the INSERT operation
-    const [insertResult] = await connection.query(insertQuery, [
-      busboardingpointData.collegeBusId,
-      busboardingpointData.academicyearId,
-      busboardingpointData.boardingPointId,
-      busboardingpointData.boardingTime,
-      busboardingpointData.dropTime
-    ]);
-
-    console.log('Data inserted successfully:', insertResult);
-  } catch (error) {
-    console.error('Error inserting data:', error); // Log the error message for debugging
-  }finally {
-    // Close the connection
-    connection.end();
-  }
-};
-
-// Connect to the database and insert data
+// Connect to the database
 connection.connect(err => {
   if (err) {
     console.error('Error connecting to the database:', err);
     return;
   }
 
-  insertData(); // Call the function to insert data
-});
+  // Perform the INSERT operation
+  connection.query(insertQuery, [
+    busboardingpointData.collegeBusId,
+    busboardingpointData.academicyearId,
+    busboardingpointData.boardingPointId,
+    busboardingpointData.boardingTime,
+    busboardingpointData.dropTime
+  ], (err, results, fields) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+    } else {
+      console.log('Data inserted successfully!');
+    }
 
+    // Close the connection
+    connection.end();
+  });
+});
