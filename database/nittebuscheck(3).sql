db1e0182bd573dc640e89334a6ad0963aa18948c
srujanshettythor <shrujanushetty@gmail.com>
2023-12-08 15:06:12 +0530

    database where added

--- database/nittebuscheck(3).sql
+++ database/nittebuscheck(3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 04, 2023 at 03:30 PM
+-- Generation Time: Dec 08, 2023 at 10:34 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -78,6 +78,15 @@ CREATE TABLE `busboardingpoints` (
   `dropTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `busboardingpoints`
+--
+
+INSERT INTO `busboardingpoints` (`busBoardingPointId`, `collegeBusId`, `academicyearId`, `boardingPointId`, `boardingTime`, `dropTime`) VALUES
+('2a33e7a5-15fe-4d9e-bbce-1bb548529084', '37902748ffbe47993', '6bc54b4e35b249e3964cf5d11970a677', '3a976c5d88354918b3733511fe83723', '10:10:17', '15:12:15'),
+('6969bf01-c6e5-4e6b-bd29-53c73c58e032', '37902748ffbe47969', '6bc54b4e35b249e3964cf5d11970a685', '3a976c5d88354918b3733511fe837216', '10:10:10', '15:12:10'),
+('73dce93c-9204-4b80-b410-94f19faf6d25', '37902748ffbe47999', '6bc54b4e35b249e3964cf5d11970a675', '3a976c5d88354918b3733511fe837218', '10:10:17', '15:12:15');
+
 -- --------------------------------------------------------
 
 --
@@ -92,16 +101,19 @@ CREATE TABLE `collegebus` (
   `purchaseDate` date NOT NULL,
   `startingPoint` varchar(70) NOT NULL,
   `endingPoint` varchar(60) NOT NULL,
-  `noOfSeats` int(60) NOT NULL
+  `noOfSeats` int(60) NOT NULL,
+  `busImage` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `collegebus`
 --
 
-INSERT INTO `collegebus` (`collegeBusId`, `busNo`, `routeNo`, `regDate`, `purchaseDate`, `startingPoint`, `endingPoint`, `noOfSeats`) VALUES
-('37902748-ffbe-4796-9', '2', '5', '0000-00-00', '0000-00-00', 'Kundapura', 'goa', 47),
-('606072d2-6e3b-4e1c-a', '4', '3', '2023-01-21', '2023-04-20', 'Mangalore', 'Nitte', 57);
+INSERT INTO `collegebus` (`collegeBusId`, `busNo`, `routeNo`, `regDate`, `purchaseDate`, `startingPoint`, `endingPoint`, `noOfSeats`, `busImage`) VALUES
+('12', '2', '4', '2023-12-18', '2023-12-12', 'bedra', 'kudla', 89, '2'),
+('37902748-ffbe-4796-9', '2', '5', '0000-00-00', '0000-00-00', 'Kundapura', 'goa', 47, '0'),
+('606072d2-6e3b-4e1c-a', '4', '3', '2023-01-21', '2023-04-20', 'Mangalore', 'Nitte', 57, '0'),
+('a9118d39-f764-456f-9', '7', '4', '2023-02-21', '2023-06-20', 'Nitte', 'udupi', 53, '2');
 
 -- --------------------------------------------------------
 
@@ -110,15 +122,23 @@ INSERT INTO `collegebus` (`collegeBusId`, `busNo`, `routeNo`, `regDate`, `purcha
 --
 
 CREATE TABLE `collegebusemployee` (
-  `collegeBusDetailsId` varchar(40) NOT NULL,
+  `collegeBusEmpId` varchar(40) NOT NULL,
+  `name` varchar(60) NOT NULL,
   `collegeBusId` varchar(40) NOT NULL,
-  `userId` varchar(40) NOT NULL,
   `designationId` varchar(40) NOT NULL,
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
   `phono` int(150) NOT NULL,
+  `empimg` varchar(1000) NOT NULL,
   `currentStatus` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `collegebusemployee`
+--
+
+INSERT INTO `collegebusemployee` (`collegeBusEmpId`, `name`, `collegeBusId`, `designationId`, `startDate`, `endDate`, `phono`, `empimg`, `currentStatus`) VALUES
+('51bf730e-6531-4556-9960-dd0b3eeccca1', 'yamuna', 'a9118d39-f764-456f-9', 'b1832cb0c66246b493d72da60cd206d0', '0000-00-00', '0000-00-00', 898989, 'yt.png', 'True');
 
 -- --------------------------------------------------------
 
@@ -152,6 +172,7 @@ CREATE TABLE `designation` (
 
 INSERT INTO `designation` (`designation_id`, `designation`, `orderNo`) VALUES
 ('6a9dc228-f904-458f-bdb0-35edddc3fe12', 'student', 1),
+('b1832cb0c66246b493d72da60cd206d0', 'driver', 0),
 ('cf18e1db-6eb3-4b0f-907f-ce3e2fb3ef0d', 'staff', 2);
 
 -- --------------------------------------------------------
@@ -200,11 +221,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`userId`, `name`, `mobileno`, `busNo`, `email`, `password`, `userImage`, `usertype_id`) VALUES
-('35c247db22ca4837a0fe225732d7e01e', 'Vikas', 9448046, 0, 'nnm23mc174@nmamit.in', 'nnm23mc174', '', '4317d1e47f6a45c39dacdad3b8c301f4'),
+('35c247db22ca4837a0fe225732d7e01e', 'sajjige', 2147483647, 9, 'sajjige@gmail.com', '1234', 'nittre.png', '4317d1e47f6a45c39dacdad3b8c301f4'),
 ('7a98aeed-9cf8-4582-87ef-597a2ce675ac', 'KUKURU', 2147483647, 7, 'sjuY@gmail.com', 'nu23mca1456', 'pathtoimgg.png', '4317d1e47f6a45c39dacdad3b8c301f4'),
 ('a4a85346-12a8-48c5-85d8-823150ce3037', 'manOJ', 2147483647, 3, 'sju@gmail.com', 'nu23mca145', 'pathtoimg.png', '4317d1e47f6a45c39dacdad3b8c301f4'),
 ('c2bff6cb897e4229b55d222ce99b6140', 'Srujan', 97400764, 1, '', '123456', '', '23ecf27394504c9583aebb614ba10510'),
-('d2244c67-19c9-4f9f-8794-6bffb518f3b3', 'srinh', 2147483647, 0, 'satju@gmail.com', '9740076457', '', '4317d1e47f6a45c39dacdad3b8c301f4'),
 ('f5fe7ff0-f506-473c-82c9-cbf0eae11127', 'rinh', 2147483647, 0, 'stju@gmail.com', '9740076357', '', '4317d1e47f6a45c39dacdad3b8c301f4'),
 ('nu23mca158', 'srujan', 97400764, 121, 'srujan@gmail.com', '123456', '', '23ecf27394504c9583aebb614ba10510');
 
@@ -226,7 +246,9 @@ CREATE TABLE `usertype` (
 
 INSERT INTO `usertype` (`usertype_id`, `usertype`, `orderNo`) VALUES
 ('23ecf27394504c9583aebb614ba10510', 'Admin', 1),
-('4317d1e47f6a45c39dacdad3b8c301f4', 'student', 2);
+('4317d1e47f6a45c39dacdad3b8c301f4', 'student', 2),
+('56d33d7538cd458b83e2279eefba4a1f', 'driver', 0),
+('79b31af46d80415884257d516c70a2b3', 'Staff', 3);
 
 --
 -- Indexes for dumped tables
@@ -263,9 +285,8 @@ ALTER TABLE `collegebus`
 -- Indexes for table `collegebusemployee`
 --
 ALTER TABLE `collegebusemployee`
-  ADD PRIMARY KEY (`collegeBusDetailsId`),
+  ADD PRIMARY KEY (`collegeBusEmpId`),
   ADD KEY `collegeBusId_FK` (`collegeBusId`),
-  ADD KEY `userId_FK` (`userId`),
   ADD KEY `designationId_FK` (`designationId`);
 
 --
